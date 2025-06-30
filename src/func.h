357d4ee0e79bacf244521f1ba87f5131b788bb36
Ruslan <alexandergromov99@gmail.com>
2025-06-30 17:41:41 +0300

    Update func.h

--- src/func.h
+++ src/func.h
@@ -4,6 +4,8 @@
 #include <openssl/ssl.h>
 #include <openssl/x509v3.h>
 
+
+
 typedef struct {
     unsigned int sslv2 : 1;
     unsigned int sslv3 : 1;
@@ -23,14 +25,16 @@ typedef struct {
     TLSProtocolSupport protocol_support;
     STACK_OF(SSL_CIPHER) *ciphers;
     X509 *cert;
+    SSL* ssl;
 } TLSCheckResult;
 
 void print_hex(const unsigned char *data, size_t len);
+void print_ciphers_from_stack(STACK_OF(SSL_CIPHER) *ciphers);
 void print_cert_info(X509 *cert);
-void print_cipher_list(STACK_OF(SSL_CIPHER) *ciphers);
 void print_protocol_support(TLSProtocolSupport *support);
 TLSCheckResult check_tls_server(const char *host, int port);
 void init_openssl();
 void cleanup_openssl();
 
 #endif
+
