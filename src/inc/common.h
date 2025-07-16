bd4e905efb6e87eadfaa89e894407737db9357b7
Ruslan <alexandergromov99@gmail.com>
2025-07-16 21:58:40 +0300

    Update common.h

--- src/inc/common.h
+++ src/inc/common.h
@@ -1,6 +1,6 @@
 #ifndef COMMON_H
 #define COMMON_H
-
+#include <stdbool.h>
 #define MAX_DOMAINS 256
 #define MAX_DOMAIN_LEN 253
 typedef struct {
@@ -8,5 +8,14 @@ typedef struct {
     int count;
     int show_help;
 } Options;
+/* Logger functions */
+void init_logger(const char* filename);
+void log_message(const char* message);
+void update_progress(int current, int total);
 
+/* Domain validation */
+bool is_valid_domain(const char *domain);
+int parse_arguments(Options *options, int argc, char **argv);
+void print_help(const char *program_name);
+bool is_network_available();
 #endif
